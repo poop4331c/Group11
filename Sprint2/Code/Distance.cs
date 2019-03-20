5fb7c352cb671291108ba8d6bf924fe295472dfa
nand03 <47265810+nand03@users.noreply.github.com>
2019-03-20 10:57:11 -0400

    Add files via upload

---
+++ Sprint2/Code/Distance.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Distance : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject startingPosition;
+    public float Distance_;
+    public Text distanceMoved;
+ 
+    void Update()
+    {
+        Distance_ = Vector3.Distance(player.transform.position, startingPosition.transform.position);
+        distanceMoved.text = calculateDistance(Distance_).ToString();
+    }
+
+    public static int calculateDistance(float distance)
+    {
+        int distanceRounded = (int)distance;
+        return distanceRounded;
+    }
+}
