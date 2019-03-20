a1f0b5ee9231c9f8ed5ff6b69b6b4ee181987ea1
nand03 <47265810+nand03@users.noreply.github.com>
2019-03-20 10:33:06 -0400

    Add files via upload

---
+++ Sprint2/Unit Tests/DistanceTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class DistanceTest
+{
+    [Test]
+    public void calculateDistance()
+    {
+        //ARRANGE
+        var distanceCalculator = new Distance();
+        var player_position = new Vector3(0.0f, 1.0f, 2.1f);
+        var startingPosition = new Vector3(0.0f,1.0f,0.6f);
+        var Distance_ = Vector3.Distance(player_position, startingPosition);
+        var expectedDistance = (int)(2.1 - 0.6);
+
+        //ACT
+        var distance = Distance.calculateDistance(Distance_);
+
+        //ASSERT
+        Assert.That(distance, Is.EqualTo(expectedDistance));
+
+        
+    }
+}
