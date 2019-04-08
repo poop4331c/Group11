7c09dd82d1153c7e529ac8c7bccf49bd58c56c98
Keyana Wright <43353330+KWright8@users.noreply.github.com>
2019-04-08 11:49:32 -0400

    Create Stats.cs

---
+++ Sprint3/code/Stats.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    private int totalScore;
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.name == "Total PowerUps")
+        {
+            GetComponent<TextMesh>().text = "Total Powerups: " + EnviormentGM.crystalPowerUpTotal;
+        }
+        if (gameObject.name == "Total Points")
+        {
+        //    GetComponent<TextMesh>().text = "Points: " + EnviormentGM.crystalPowerUpTotal;
+         //   GetComponent<TextMesh>().text = "Distance: " + GameManager.finalScore;
+            GetComponent<TextMesh>().text = "Total Score: " + (int)GameManager.finalScore;
+        }
+    }
+}
